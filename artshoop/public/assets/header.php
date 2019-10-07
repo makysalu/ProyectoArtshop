c99687486c373f62b08ebe709e856567a4d0b8bd
makysalu <44227511+makysalu@users.noreply.github.com>
2019-10-07 13:16:14 +0200

    Add files via upload

---
+++ artshoop/public/assets/header.php
@@ -0,0 +1,42 @@
+<header>
+    <span>
+        <figure>
+            <img id="logo" src="img/logo.png">
+        </figure>
+    </span>
+    <span>
+        <h1>ArtShop</h1>
+    </span>
+    <nav>
+        <ul>
+            <li>Ayuda</li>
+            <li>Contatenos</li>
+            <li>Registrarse</li>
+            <li>Acceder</li>
+        </ul>
+    </nav>
+</header>
+<nav>
+   <ul>
+       <li>Novedades
+           <ul>
+               <li>Pintura</li>
+               <li>Escultura</li>
+               <li>Dibujo</li>
+               <li>Fotografia</li>
+           </ul>
+       </li>
+       <li>Temáticas
+           <ul>
+               <li>Abstracto</li>
+               <li>Urbano</li>
+               <li>Paisaje</li>
+               <li>Retrato</li>
+               <li>Bodegones</li>
+               <li>Animales</li>
+           </ul>
+       </li>
+       <li>Artistas</li>
+       <li>Colecciones</li>
+   </ul> 
+</nav>
---
+++ artshoop/public/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <script src="./js/modernizr-custom.js"></script>
+    <link rel="stylesheet" href="./css/normalize.css">
+    <link rel="stylesheet" href="./css/font.css">
+    <link rel="stylesheet" href="./css/header.css">
+    <link rel="stylesheet" href="./css/style.css">
+    <title>artshop</title>
+</head>
+<body>
+    <?php include "./assets/header.php"; ?>
+    <figure id="imgportada">
+        <img src="img/pintura.jpg" alt="">
+    </figure>
+</body>
+</html>
