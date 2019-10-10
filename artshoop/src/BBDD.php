92f39213a8f690eedda40515f5eb97056f634027
diego <diego_maky@hotmail.com>
2019-10-10 19:24:54 +0200

    Update BBDD.php

--- artshoop/src/BBDD.php
+++ artshoop/src/BBDD.php
@@ -6,7 +6,7 @@
     }
 
     public function conexion(){
-      $this->conexion = new mysqli("localhost", "root", "", "artshop");
+      $this->conexion = new  mysqli("localhost", "root", "", "artshop");
         if ($this->conexion->connect_errno) {
             echo "Fallo al conectar a MySQL: (" . $this->conexion->connect_errno . ") " . $this->conexion->connect_error;
             $error=false;
